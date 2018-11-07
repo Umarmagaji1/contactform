e20e1b12ba077982d9ad5b34b1578958f33b2c68
unknown <magaji_u@yahoo.com>
2018-11-07 14:30:28 +0100

    Updates

---
+++ precache-manifest.2afcf57c770b50f0532afe2b5f31d190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffd1c751c4a8991fd956",
+    "url": "/contactform/static/js/runtime~main.ffd1c751.js"
+  },
+  {
+    "revision": "c52854b4581433970b7f",
+    "url": "/contactform/static/js/main.c52854b4.chunk.js"
+  },
+  {
+    "revision": "f014e830ddc7b7521914",
+    "url": "/contactform/static/js/1.f014e830.chunk.js"
+  },
+  {
+    "revision": "c52854b4581433970b7f",
+    "url": "/contactform/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "f36d4371ea6422dd34554ad3857e0028",
+    "url": "/contactform/index.html"
+  }
+];
